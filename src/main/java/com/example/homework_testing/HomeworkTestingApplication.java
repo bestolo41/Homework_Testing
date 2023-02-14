f4142b8109de16bf76ea6f04c96e69a25bd4e1ad
BULAT <ibragimovbi@yandex.ru>
2023-02-14 19:39:39 +0300

    Initial commit

---
+++ src/main/java/com/example/homework_testing/HomeworkTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkTestingApplication.class, args);
+    }
+
+}
